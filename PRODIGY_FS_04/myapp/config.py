2066613e9e714197312ea543d9bf35b00fb85790
poojaasri balakumar <poojaasribalakumar@gmail.com>
2024-12-23 04:25:07 +0530

    config.py

--- PRODIGY_FS_04/myapp/config.py
+++ PRODIGY_FS_04/myapp/config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Poojaasri@2004'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
